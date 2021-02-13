3f92402ba298ddec7d8a45e8f281114a9bb0c1e5
残月战歌 <hc980729@gmail.com>
2021-02-13 17:25:09 +0800

    update 2021.02.13

---
+++ src/done/T1423.java
@@ -0,0 +1,51 @@
+package done;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class T1423 {
+	
+	@Test
+	public void test() {
+		int[] cardPoints = {100,40,17,9,73,75};
+		
+		assertEquals(maxScore(cardPoints, 3), 248);
+	}
+	
+	
+	public int maxScore(int[] cardPoints, int k) {
+		int res = 0;
+		
+		int sum = 0;
+		for(int card : cardPoints) {
+			sum += card;
+		}
+		
+		int window = cardPoints.length - k;
+		int left = 1, right = window;
+		
+		int tempSum = 0;
+		for(int i = 0; i < right; i++) {
+			tempSum += cardPoints[i];
+		}
+		res = tempSum;
+		
+//		System.out.println("start: left:" + cardPoints[0] + " right:" + cardPoints[window-1]);
+//		System.out.println("tempSum:" + tempSum);
+		while(right < cardPoints.length) {
+//			System.out.println("now: left:" + cardPoints[left] + " right:" + cardPoints[right]);
+			
+			tempSum = tempSum - cardPoints[left - 1] + cardPoints[right];
+//			System.out.println("tempSum:" + tempSum);
+			
+			res = Math.min(tempSum, res);
+//			System.out.println("min:" + tempSum);
+			
+			left++;
+			right++;
+		}
+		
+		return sum - res;
+    }
+}
---
+++ src/done/T448.java
@@ -0,0 +1,42 @@
+package done;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class T448 {
+	
+	@Test
+	public void test() {
+		int[] nums = {4,3,2,7,8,2,3,1};
+		
+		assertEquals((int)findDisappearedNumbers(nums).get(0), 5);		
+	}
+	
+	/*
+	 * 在原数组上修改 令出现的数对应的位置都加上len 
+	 * 最后对应位置不大于len则没出现过
+	 */
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> res = new ArrayList<Integer>();
+		
+		int len = nums.length;
+		for(int i = 0; i < len; i++) {
+			//防止数字出现过，将对应位置对 n 取模来还原出它本来的值
+			int x = (nums[i] - 1) % len;
+			nums[x] += len;
+		}
+		
+		for(int i = 0; i < len; i++) {
+			if(nums[i] <= len) {
+				res.add(i + 1);
+			}
+		}
+		
+		return res;
+    }
+	
+}
